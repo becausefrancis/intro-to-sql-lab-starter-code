61f0b371fd1134f83103f544a86d4dac6b41fe85
becausefrancis <jimenez.francis92@gmail.com>
2025-02-13 05:03:56 -0600

    Done.

--- clues.sql
+++ clues.sql
@@ -1,33 +1,45 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
--- Write SQL query here
+SELECT name FROM countries WHERE region = 'Southern Europe' ORDER BY population DESC;
+
+--returned list of countries with Holy See (Vatican City State) as country with lowest population
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
--- Write SQL query here
+SELECT countries.code AS code, countrylanguages.language AS language FROM countries JOIN countrylanguages ON countries.code = countrylanguages.countrycode AND countries.name = 'Holy See (Vatican City State)' AND countrylanguages.isofficial = TRUE;
 
+--returned Italian as the Holy See's official language
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
--- Write SQL query here
+SELECT countries.code, countries.name AS name, countrylanguages.language AS language FROM countries JOIN countrylanguages 
+ON countries.code = countrylanguages.countrycode AND countrylanguages.language = 'Italian' AND percentage = 100;
+
+-- returned San Marino
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
--- Write SQL query here
+SELECT name FROM cities WHERE countrycode = 'SMR' AND name != 'San Marino';
+
+--returned Serravalle
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
--- Write SQL query here
+SELECT cities.name AS city, cities.countrycode AS countrycode, countries.name AS country, countries.region AS region FROM cities JOIN countries ON cities.countrycode = countries.code AND cities.name LIKE 'Serra%' and countries.region = 'South America';
+
+--returned Serra, Brazil
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
--- Write SQL query here
+SELECT countries.code AS countrycode, countries.capital AS capitalindex, cities.countrycode, cities.name, cities.id AS cityid FROM countries JOIN cities ON countries.code=cities.countrycode AND countries.code = 'BRA' AND countries.capital = cities.id;
+
+--returned Brasilia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -42,3 +54,7 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT cities.name AS city, cities.population, cities.countrycode AS citycountrycode, countries.code AS countrycode, countries.name AS country FROM cities JOIN countries ON cities.countrycode = countries.code AND cities.population = 91084;
+
+--She's in Santa Monica, California, got her!
